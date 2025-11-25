5b37fecc4b0fbaa69e9fe56aa3f4828474d7f454
Regina Rakhimova <168841400+rrakhimovva@users.noreply.github.com>
2025-11-25 18:49:57 +0300

    Add files via upload

---
+++ lab5/Task1.sql
@@ -0,0 +1,58 @@
+﻿-- Создаем пользователей 
+CREATE LOGIN User_Manager WITH PASSWORD = '1234567'
+CREATE USER User_Manager FOR LOGIN User_Manager
+
+CREATE LOGIN User_Employee WITH PASSWORD = '1234567'
+CREATE USER User_Employee FOR LOGIN User_Employee
+
+-- Создаем две роли
+CREATE ROLE Role_Manager
+CREATE ROLE Role_Employee
+
+-- Добавляем пользователей к ролям 
+ALTER ROLE Role_Manager ADD MEMBER User_Manager
+ALTER ROLE Role_Employee ADD MEMBER User_Employee
+
+-- Выдаем права руководителю 
+GRANT SELECT, INSERT, UPDATE, DELETE ON Manufacturer TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON Cashier TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON Disease TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON MedicationType TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON Sales TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON MedicineBatch TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON MedicationType_Disease TO Role_Manager
+GRANT SELECT, INSERT, UPDATE, DELETE ON MedicineBatch_Sales TO Role_Manager
+
+GRANT EXECUTE ON CalculateAverageMedicationsCount TO Role_Manager
+GRANT EXECUTE ON FindCheapestAnalog TO Role_Manager
+GRANT EXECUTE ON GetExpiringMedications1 TO Role_Manager
+GRANT EXECUTE ON GetMedicationsByActiveSubstance TO Role_Manager
+GRANT EXECUTE ON GetSalesWithAboveAverageMedications TO Role_Manager
+GRANT SELECT ON GetMedicationsForDisease TO Role_Manager
+GRANT SELECT ON GetSoldMedicationsByDate TO Role_Manager
+GRANT EXECUTE ON GetDailyRevenue TO Role_Manager
+
+GRANT SELECT ON Manufacturer TO Role_Manager WITH GRANT OPTION
+GRANT EXECUTE ON GetDailyRevenue TO Role_Manager WITH GRANT OPTION
+
+-- Выдаем права сотруднику
+GRANT SELECT ON MedicationType TO Role_Employee
+GRANT SELECT ON MedicineBatch TO Role_Employee
+GRANT SELECT ON Disease TO Role_Employee
+GRANT SELECT ON MedicationType_Disease TO Role_Employee
+GRANT SELECT ON Manufacturer TO Role_Employee
+
+GRANT SELECT, INSERT ON Sales TO Role_Employee
+GRANT SELECT, INSERT ON MedicineBatch_Sales TO Role_Employee
+
+DENY SELECT ON Cashier TO Role_Employee
+
+REVOKE SELECT ON Disease FROM Role_Employee
+
+GRANT EXECUTE ON GetDailyRevenue TO Role_Employee
+GRANT SELECT ON GetSoldMedicationsByDate TO Role_Employee
+GRANT SELECT ON GetMedicationsForDisease TO Role_Employee
+
+DENY UPDATE, DELETE ON Sales TO Role_Employee
+DENY UPDATE, DELETE ON MedicineBatch_Sales TO Role_Employee
+DENY UPDATE, DELETE ON MedicineBatch TO Role_Employee
---
+++ lab5/Task1_example.sql
@@ -0,0 +1,25 @@
+﻿EXECUTE AS USER = 'User_Manager'
+
+SELECT * FROM Manufacturer         
+SELECT * FROM Cashier              
+EXEC GetExpiringMedications1      
+
+GRANT SELECT ON Disease TO User_Employee  
+
+REVERT
+
+
+EXECUTE AS USER = 'User_Employee'
+
+
+SELECT * FROM MedicationType                    
+SELECT * FROM Manufacturer                   
+SELECT * FROM GetMedicationsForDisease('Грипп') 
+SELECT * FROM Disease                  
+
+
+SELECT * FROM Cashier                          
+UPDATE Sales SET amount = 2000 WHERE id = 1    
+DELETE FROM Sales WHERE id = 1                  
+
+REVERT
