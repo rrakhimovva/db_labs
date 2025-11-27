b7addf3cbc67633737dc79f91fa850c6990e60b0
Regina Rakhimova <168841400+rrakhimovva@users.noreply.github.com>
2025-11-27 17:40:11 +0300

    Update Task1_example.sql

--- lab5/Task1_example.sql
+++ lab5/Task1_example.sql
@@ -1,25 +1,23 @@
-﻿EXECUTE AS USER = 'User_Manager'
+-- Примеры для руководителя
+EXECUTE AS USER = 'User_Manager'
 
 SELECT * FROM Manufacturer         
 SELECT * FROM Cashier              
 EXEC GetExpiringMedications1      
 
-GRANT SELECT ON Disease TO User_Employee  
-
 REVERT
 
-
+-- Примеры для сотрудника
 EXECUTE AS USER = 'User_Employee'
-
 
 SELECT * FROM MedicationType                    
 SELECT * FROM Manufacturer                   
 SELECT * FROM GetMedicationsForDisease('Грипп') 
-SELECT * FROM Disease                  
-
-
-SELECT * FROM Cashier                          
+                     
 UPDATE Sales SET amount = 2000 WHERE id = 1    
 DELETE FROM Sales WHERE id = 1                  
 
 REVERT
+
+
+
